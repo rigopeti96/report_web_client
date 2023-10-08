942d3268c2dc06552f6027af61bfe6f8792a0d4c
Péter Attila Rigó <61515182+rigopeti96@users.noreply.github.com>
2023-10-08 11:19:39 +0200

    auth service works and prepared to file upload

--- report-web-application/report-web-application/src/app/shared/auth.service.ts
+++ report-web-application/report-web-application/src/app/shared/auth.service.ts
@@ -16,21 +16,13 @@ export class AuthService {
   headers = new HttpHeaders().set('Content-Type', 'application/json');
   currentUser = {};
   constructor(private http: HttpClient, public router: Router) {}
-  // Sign-up
-  signUp(user: User): Observable<any> {
-    let api = `${this.endpoint}/register-user`;
-    return this.http.post(api, user).pipe(catchError(this.handleError));
-  }
   // Sign-in
   signIn(user: User) {
     return this.http
       .post<any>(`${this.endpoint}/signin`, user)
       .subscribe((res: any) => {
         localStorage.setItem('access_token', res.token);
-        this.getUserProfile(res._id).subscribe((res) => {
-          this.currentUser = res;
-          this.router.navigate(['user-profile/' + res.msg._id]);
-        });
+        this.router.navigate(['user-profile/' + res.msg._id]);
       });
   }
   getToken() {
@@ -45,16 +37,6 @@ export class AuthService {
     if (removeToken == null) {
       this.router.navigate(['log-in']);
     }
-  }
-  // User profile
-  getUserProfile(id: any): Observable<any> {
-    let api = `${this.endpoint}/user-profile/${id}`;
-    return this.http.get(api, { headers: this.headers }).pipe(
-      map((res) => {
-        return res || {};
-      }),
-      catchError(this.handleError)
-    );
   }
   // Error
   handleError(error: HttpErrorResponse) {
